2889eed2280bbfd703293e791f64936005868bce
dabananda <dmitraofficial@gmail.com>
2022-03-17 18:49:35 +0600

    Basic packages installation and setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+function App() {
+  return (
+    <div className="container">
+      
+    </div>
+  )
+}
+
+export default App
